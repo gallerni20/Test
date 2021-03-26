4398e2b3f64ab135815f3e3a4b9f048bd537f357
gallerni20 <80049986+gallerni20@users.noreply.github.com>
2021-03-26 14:25:45 +0100

    Delete 1. UE.py

--- 1. UE.py
+++
@@ -1,9 +0,0 @@
-print ('Hello user!')
-
-zahl1= int(input('Gib die erste Zahl ein: '))
-zahl2= int(input('Gib eine zweite Zahl ein: '))
-
-print ('Summe: ', zahl1+zahl2)
-print ('Differenz: ', zahl1-zahl2)
-print ('Produkt: ', zahl1*zahl2)
-print ('Quotient: ', float(zahl1/zahl2))
